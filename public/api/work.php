24a44fea327a0037f45027cc45ca7a95d7508868
suyashkhemka <suyashkhemka123@gmail.com>
2018-10-03 11:14:38 -0400

    v1

--- public/api/work.php
+++ public/api/work.php
@@ -1,7 +1,20 @@
 <?php
-// Get the taskId from URL params
+
+require '../../app/common.php';
+
 $taskId = intval($_GET['taskId'] ?? 0);
-// Fetch the work from the db
-$work = Work::findByTaskId($taskId);
-// convert to json and print
-echo json_encode($work);
+
+if ($taskId < 1) {
+  throw new Exception('Invalid Task ID');
+}
+
+
+// 1. Go to the database and get all work associated with the $taskId
+$workArr = Work::getWorkByTaskId($taskId);
+
+// 2. Convert to JSON
+$json = json_encode($workArr, JSON_PRETTY_PRINT);
+
+// 3. Print
+header('Content-Type: application/json');
+echo $json;
