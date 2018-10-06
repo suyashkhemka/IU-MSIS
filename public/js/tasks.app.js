8bf34becea16bcefddcb0018070cc08e767d7354
suyashkhemka <suyashkhemka123@gmail.com>
2018-10-06 19:55:19 -0400

    POST to work API

--- public/js/tasks.app.js
+++ public/js/tasks.app.js
@@ -20,30 +20,39 @@ var tasksApp = new Vue({
   },
   computed: {
     workSpan () {
-      return moment(this.workForm.stop)
-             .diff(moment(this.workForm.start), 'hours', true)
+      return moment(this.workForm.stop + ' ' + this.workForm.stop_time)
+             .diff(moment(this.workForm.start + ' ' + this.workForm.start_time), 'hours', true)
              .toFixed(1);
     }
   },
   methods: {
     handleWorkForm(e) {
-      e.preventDefault();
-
       // TODO: Check validity
+      if (this.workSpan < 0) {
+        console.error('Hours must be positive')
+      }
 
-      console.log(e);
-
-      // TODO: Calculate hours
-      // something like:  moment.duration(end.diff(startTime)).asHours();
-
-      //TODO: clone workForm
+      // Build the JSON to send
+      this.workForm.task_id = this.taskId;
+      this.workForm.hours = this.workSpan;
+      this.workForm.start_date = this.workForm.start + ' ' + this.workForm.start_time
       const s = JSON.stringify(this.workForm);
-      //TODO: POST to remote server
-      // fetch(url,)
-      // .then()
+      console.log(s);
 
-      //TODO: Append result
-      this.work.push(JSON.parse(s));
+      // POST to remote server
+      fetch('api/work.php', {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        },
+        body: s // body data type must match "Content-Type" header
+      })
+      .then( response => response.json() )
+      .then( json => {this.work.push(json)})
+      .catch( err => {
+        console.error('WORK POST ERROR:');
+        console.error(err);
+      })
 
       // Reset workForm
       this.workForm = this.getEmptyWorkForm();
@@ -54,20 +63,26 @@ var tasksApp = new Vue({
     diffAsHours() {
       return 0 //moment().duration(end.diff(startTime)).asHours();
     },
-    datetimeFormat(d) {
+    dateFormat(d) {
       d = d || moment();
-      return moment(d).format('YYYY-MM-DD[T]HH:MM');
+      return moment(d).format('YYYY-MM-DD');
+    },
+    timeFormat(d) {
+      d = d || moment();
+      return moment(d).format('HH:mm');
     },
     getEmptyWorkForm() {
       return {
-        start: this.datetimeFormat(),
-        stop: this.datetimeFormat(),
-        teamList: null,
+        start: this.dateFormat(),
+        start_time: this.timeFormat(),
+        stop: this.dateFormat(),
+        stop_time: this.timeFormat(),
+        team_id: null,
         completion_estimate: 0
       }
     },
     prettyDate(d) {
-      return moment(d).format('YYYY-MM-DD HH:MM')
+      return moment(d).format('YYYY-MM-DD HH:mm')
     }
   },
   created () {
@@ -77,6 +92,7 @@ var tasksApp = new Vue({
     // Do data fetch
     const url = new URL(window.location.href);
     const taskId = url.searchParams.get('taskId');
+    this.taskId = taskId;
     console.log('Task: '+ taskId);
 
     if (!taskId) {
